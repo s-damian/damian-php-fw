3de50a0d952d171e003ef083eac4c3add64ff690
s-damian <contact@damian-freelance.fr>
2024-05-03 18:53:30 +0300

    Update Cookie class

--- src/DamianPhp/Contracts/Http/Request/CookieInterface.php
+++ src/DamianPhp/Contracts/Http/Request/CookieInterface.php
@@ -15,14 +15,14 @@ interface CookieInterface
      * Créer un cookie.
      *
      * @param string $name - Nom du cookie.
-     * @param int $value - Valeur du cookie.
+     * @param string $value - Valeur du cookie.
      * @param string|null $minutes - Durée du cookie, temps après lequel le cookie expire. A spécifier en minuttes.
      * @param string $path - Le chemin sur le serveur sur lequel le cookie sera disponible.
      * @param string|null $domain - Le domaine pour lequel le cookie est disponible.
      * @param bool|null $secure - Indique si le cookie doit uniquement être transmis à travers une connexion sécurisée HTTPS depuis le client.
      * @param bool|null $httponly - Lorsque ce paramètre vaut TRUE, le cookie ne sera accessible que par le protocole HTTP (ne sera pas éditable en JS...).
      */
-    public function put(string $name, $value, int $expire = null, string $path = '/', string $domain = '', bool $secure = false, bool $httpOnly = true);
+    public function put(string $name, string $value = '', int $expire = null, string $path = '/', string $domain = '', bool $secure = false, bool $httpOnly = true);
 
     public function has(string $name): bool;
 
--- src/DamianPhp/Http/Request/Cookie.php
+++ src/DamianPhp/Http/Request/Cookie.php
@@ -28,14 +28,14 @@ class Cookie implements CookieInterface
      * Créer un cookie.
      *
      * @param string $name - Nom du cookie.
-     * @param int $value - Valeur du cookie.
+     * @param string $value - Valeur du cookie.
      * @param string|null $minutes - Durée du cookie, temps après lequel le cookie expire. A spécifier en minuttes.
      * @param string $path - Le chemin sur le serveur sur lequel le cookie sera disponible.
      * @param string|null $domain - Le domaine pour lequel le cookie est disponible.
      * @param bool|null $secure - Indique si le cookie doit uniquement être transmis à travers une connexion sécurisée HTTPS depuis le client.
      * @param bool|null $httponly - Lorsque ce paramètre vaut TRUE, le cookie ne sera accessible que par le protocole HTTP (ne sera pas éditable en JS...).
      */
-    public function put(string $name, $value, int $minutes = null, string $path = null, string $domain = null, bool $secure = null, bool $httponly = null)
+    public function put(string $name, string $value = '', int $minutes = null, string $path = null, string $domain = null, bool $secure = null, bool $httponly = null)
     {
         if ($minutes === 0) {
             $expireValue = 0;
@@ -79,6 +79,6 @@ class Cookie implements CookieInterface
 
         $domainValue = $domain ?? Helper::config('cookie')['domain'];
 
-        $this->put($name, null, -2628000, $pathValue, $domainValue);
+        $this->put($name, '', -2628000, $pathValue, $domainValue);
     }
 }
